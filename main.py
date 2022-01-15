3cf25c5b471b4120f1af35ab5d1e5c4705e843a4
Andrei Albu <andrei.albu03@gmail.com>
2022-01-15 10:49:29 -0500

    commit

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
 import discord
 
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('We have logged in a {0.user}'.format(client))
+    return
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith("$hello"):
+        await message.channel.send("Hello!")
+
+
+
+try :
+    client.run("OTMxOTMxNTUyNTcxNjA1MDQy.YeLm5A.uwvgcM9dZOkCPyY4C_vdn2mZHbc")
+except discord.errors.HTTPException:
+    print("Token is broken, get a new one") 
